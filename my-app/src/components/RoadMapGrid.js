41cbf59b55a6fd76dad18a1ba29268702e15fbbd
brunogleite <brunoleite6o@gmail.com>
2022-04-17 10:35:56 +0100

    roadmap grid
    roadmap component finished

--- my-app/src/components/RoadMapGrid.js
+++ my-app/src/components/RoadMapGrid.js
@@ -1,38 +1,54 @@
 import { Box, Flex, Grid, Text, Heading } from "@chakra-ui/react";
+import { useContext } from "react";
+import DataContext from "../context/data/dataContext";
 import RoadmapComponent from "./RoadmapComponent";
 
 
 export default function RoadMapGrid(){
+    const dataContext = useContext(DataContext);
+    const {requests} = dataContext
+
+    //make three variables to store and filter for each category : planned, in-progress, live
+    const plannedItems = requests.filter(item => item.status === "planned"); 
+
+    const inProgressItems =  requests.filter(item => item.status === "in-progress");
+
+    const liveItems = requests.filter(item => item.status === "live"); 
+
 
     return(
         <Grid templateColumns="repeat(3, 1fr)" gridColumnGap="8">
             <Box w="full">
                 <Box mt="12" mb="8">
-                    <Heading as="h3" fontWeight="medium" color="darkBlue10" fontSize="headingXS">Planned (2)</Heading>
+                    <Heading as="h3" fontWeight="medium" color="darkBlue10" fontSize="headingXS">Planned ({plannedItems.length})</Heading>
                     <Text color="greyColor" fontSize="bodyM">Ideas prioritized for research</Text>
                 </Box>
                 <Box>
-                    {/*Componente*/}
-                    <RoadmapComponent />
-                    {/*Componente*/}
+                    {plannedItems?.map((item) => {
+                        return <RoadmapComponent id={item.id} product={item} />
+                    })}
                 </Box>
             </Box>
             <Box w="full">
                 <Box mt="12" mb="8">
-                    <Heading as="h3" fontWeight="medium" color="darkBlue10" fontSize="headingXS">Planned (2)</Heading>
+                    <Heading as="h3" fontWeight="medium" color="darkBlue10" fontSize="headingXS">In-Progress ({inProgressItems.length})</Heading>
                     <Text color="greyColor" fontSize="bodyM">Ideas prioritized for research</Text>
                 </Box>
                 <Box>
-                <RoadmapComponent />
+                    {inProgressItems?.map((item) => {
+                        return <RoadmapComponent id={item.id} product={item} />
+                    })}
                 </Box>
             </Box>
             <Box w="full">
                 <Box mt="12" mb="8">
-                    <Heading as="h3" fontWeight="medium" color="darkBlue10" fontSize="headingXS">Planned (2)</Heading>
+                    <Heading as="h3" fontWeight="medium" color="darkBlue10" fontSize="headingXS">Live ({liveItems.length})</Heading>
                     <Text color="greyColor" fontSize="bodyM">Ideas prioritized for research</Text>
                 </Box>
                 <Box>
-                <RoadmapComponent />
+                    {liveItems?.map((item) => {
+                        return <RoadmapComponent id={item.id} product={item} />
+                    })}
                 </Box>
             </Box>
         </Grid>
--- my-app/src/components/RoadmapComponent.js
+++ my-app/src/components/RoadmapComponent.js
@@ -1,31 +1,31 @@
 import { Box, Flex, Text, Heading } from "@chakra-ui/react";
 
 
-export default function RoadmapComponent(){
+export default function RoadmapComponent({product}){
 
     return (
         <Box 
             pos="relative" 
-            _before={{content: "''", bgColor: "red.500", w:"full", h:"6px", pos:"absolute", top:"0", left:"0", borderRadius:"5px 5px 0 0"}} 
+            _before={{content: "''", bgColor: product.status === "live" ? "lightBlue": product.status === "planned" ? "orangeColor": "primaryColor" , w:"full", h:"6px", pos:"absolute", top:"0", left:"0", borderRadius:"5px 5px 0 0"}} 
             p="8" 
             bgColor="white" 
             borderRadius="sm" 
             w="full">
                 <Box>
-                    <Flex>
-                        <Box maxW="8px" minH="8px" borderRadius="full" bgColor="red.500"></Box>
-                        <Text>Planned</Text>
+                    <Flex alignItems="center">
+                        <Box maxW="8px" w="full" h="8px" borderRadius="full" mr="4" bgColor={product.status === "live" ? "lightBlue": product.status === "planned" ? "orangeColor": "primaryColor"}></Box>
+                        <Text>{product.status}</Text>
                     </Flex>
                     <Box mt="2" mb="4" maxW="286px">
-                    <Heading as="h3" fontSize="headingXS" fontWeight="medium">More comprehensive reports</Heading>
-                        <Text fontSize="bodyM" color="greyColor">It would be great to see a more detailed breakdown of solutions.</Text>
-                     </Box>
+                        <Heading as="h3" fontSize="headingXS" fontWeight="medium">{product.title}</Heading>
+                        <Text fontSize="bodyM" color="greyColor">{product.description}</Text>
+                    </Box>
                 </Box>
                 <Box>
                     <Box mb="4">Feature</Box>
                     <Flex>
-                        <Box>123</Box>
-                        <Box ml="auto">2</Box>
+                        <Box>{product.upvotes}</Box>
+                        <Box ml="auto">{product.comments === undefined ? 0 : product.comments.length}</Box>
                     </Flex>
                 </Box>
             </Box>
